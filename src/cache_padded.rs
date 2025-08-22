ff86e4980acb36c66f43404d8899398106f2da25
Eugene Usachev <usachblog@gmail.com>
2025-08-22 18:00:58 +0300

    Update docs and the `generate_cache_padded_type` macro

--- src/cache_padded.rs
+++ src/cache_padded.rs
@@ -10,7 +10,8 @@ use std::sync::atomic::{
     target_arch = "arm64ec",
     target_arch = "powerpc64",
 ))]
-const ALIGN: usize = 128;
+/// A cache line size for the architecture that the application is compiled for.
+pub const CACHE_LINE_SIZE: usize = 128;
 
 #[cfg(any(
     target_arch = "arm",
@@ -21,13 +22,16 @@ const ALIGN: usize = 128;
     target_arch = "sparc",
     target_arch = "hexagon",
 ))]
-const ALIGN: usize = 32;
+/// A cache line size for the architecture that the application is compiled for.
+pub const CACHE_LINE_SIZE: usize = 32;
 
 #[cfg(target_arch = "m68k")]
-const ALIGN: usize = 16;
+/// A cache line size for the architecture that the application is compiled for.
+pub const CACHE_LINE_SIZE: usize = 16;
 
 #[cfg(target_arch = "s390x")]
-const ALIGN: usize = 256;
+/// A cache line size for the architecture that the application is compiled for.
+pub const CACHE_LINE_SIZE: usize = 256;
 
 #[cfg(not(any(
     target_arch = "x86_64",
@@ -44,28 +48,33 @@ const ALIGN: usize = 256;
     target_arch = "m68k",
     target_arch = "s390x",
 )))]
-const ALIGN: usize = 64;
+/// A cache line size for the architecture that the application is compiled for.
+pub const CACHE_LINE_SIZE: usize = 64;
 
+/// Generates a cache padded type. 
+/// It accepts a name of the new type, the inner type and the default function.
+/// 
+/// The new type can be dereferenced to the inner type.
 #[macro_export]
-macro_rules! generate_cache_padded_atomic {
-    ($name:ident, $atomic:ident) => {
-        /// Cache padded atomic. Can be dereferenced to the inner atomic.
+macro_rules! generate_cache_padded_type {
+    ($name:ident, $atomic:ident, $default:block) => {
+        /// Cache padded inner type. Can be dereferenced to the inner type.
         pub struct $name {
-            atomic: $atomic,
+            inner_type: $atomic,
             _align: std::mem::MaybeUninit<
-                [u8; if size_of::<$atomic>() > $crate::cache_padded::ALIGN {
+                [u8; if size_of::<$atomic>() > $crate::cache_padded::CACHE_LINE_SIZE {
                     0
                 } else {
-                    $crate::cache_padded::ALIGN - size_of::<$atomic>()
+                    $crate::cache_padded::CACHE_LINE_SIZE - size_of::<$atomic>()
                 }],
             >,
         }
 
         impl $name {
-            /// Creates a new cache padded atomic.
+            /// Creates a new cache padded inner type.
             pub const fn new() -> Self {
                 Self {
-                    atomic: $atomic::new(0),
+                    inner_type: $default,
                     _align: std::mem::MaybeUninit::uninit(),
                 }
             }
@@ -75,13 +84,13 @@ macro_rules! generate_cache_padded_atomic {
             type Target = $atomic;
 
             fn deref(&self) -> &Self::Target {
-                &self.atomic
+                &self.inner_type
             }
         }
 
         impl std::ops::DerefMut for $name {
             fn deref_mut(&mut self) -> &mut Self::Target {
-                &mut self.atomic
+                &mut self.inner_type
             }
         }
 
@@ -93,14 +102,14 @@ macro_rules! generate_cache_padded_atomic {
     };
 }
 
-generate_cache_padded_atomic!(CachePaddedAtomicU8, AtomicU8);
-generate_cache_padded_atomic!(CachePaddedAtomicU16, AtomicU16);
-generate_cache_padded_atomic!(CachePaddedAtomicU32, AtomicU32);
-generate_cache_padded_atomic!(CachePaddedAtomicU64, AtomicU64);
-generate_cache_padded_atomic!(CachePaddedAtomicUsize, AtomicUsize);
+generate_cache_padded_type!(CachePaddedAtomicU8, AtomicU8, { AtomicU8::new(0) });
+generate_cache_padded_type!(CachePaddedAtomicU16, AtomicU16, { AtomicU16::new(0) });
+generate_cache_padded_type!(CachePaddedAtomicU32, AtomicU32, { AtomicU32::new(0) });
+generate_cache_padded_type!(CachePaddedAtomicU64, AtomicU64, { AtomicU64::new(0) });
+generate_cache_padded_type!(CachePaddedAtomicUsize, AtomicUsize, { AtomicUsize::new(0) });
 
-generate_cache_padded_atomic!(CachePaddedAtomicI8, AtomicI8);
-generate_cache_padded_atomic!(CachePaddedAtomicI16, AtomicI16);
-generate_cache_padded_atomic!(CachePaddedAtomicI32, AtomicI32);
-generate_cache_padded_atomic!(CachePaddedAtomicI64, AtomicI64);
-generate_cache_padded_atomic!(CachePaddedAtomicIsize, AtomicIsize);
+generate_cache_padded_type!(CachePaddedAtomicI8, AtomicI8, { AtomicI8::new(0) });
+generate_cache_padded_type!(CachePaddedAtomicI16, AtomicI16, { AtomicI16::new(0) });
+generate_cache_padded_type!(CachePaddedAtomicI32, AtomicI32, { AtomicI32::new(0) });
+generate_cache_padded_type!(CachePaddedAtomicI64, AtomicI64, { AtomicI64::new(0) });
+generate_cache_padded_type!(CachePaddedAtomicIsize, AtomicIsize, { AtomicIsize::new(0) });
