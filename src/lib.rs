463f306ef01f774869ba358a0a76ab6234967c95
Eugene Usachev <usachblog@gmail.com>
2025-08-22 17:49:37 +0300

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! - The [`hints module`](hints) provides hints to the compiler that affects how code
 //!   should be emitted or optimized.
 //! - The [`backoff module`](backoff) provides the [`Backoff`](backoff::Backoff) structure.
-//! - The [`cache_padded module`](cache padded) provides cache-padded atomics types and
+//! - The [`cache_padded module`](cache_padded) provides cache-padded atomics types and
 //!   the [`generate_cache_padded_atomic`] macro.
 //! - The [`light_arc module`](light arc) provides the [`LightArc`](light_arc::LightArc) type.
 //! - The [`instant module`](instant) provides the [`OrengineInstant`](instant::OrengineInstant)
